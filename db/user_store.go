0fcd557301581b941afe0fe87a3bc4b6dfddd9a2
hrshshrma <totheharshsharma@gmail.com>
2024-03-23 13:45:25 +0530

    Hotel Reservation System

---
+++ db/user_store.go
@@ -0,0 +1,140 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/fulltimegodev/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const userColl = "users"
+
+type Map map[string]any
+
+type Dropper interface {
+	Drop(context.Context) error
+}
+
+type UserStore interface {
+	Dropper
+
+	GetUserByEmail(context.Context, string) (*types.User, error)
+	GetUserByID(context.Context, string) (*types.User, error)
+	GetUsers(context.Context) ([]*types.User, error)
+	InsertUser(context.Context, *types.User) (*types.User, error)
+	DeleteUser(context.Context, string) error
+	UpdateUser(ctx context.Context, filter Map, params types.UpdateUserParams) error
+}
+
+type MongoUserStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+}
+
+func NewMongoUserStore(client *mongo.Client) *MongoUserStore {
+	dbname := os.Getenv(MongoDBNameEnvName)
+	return &MongoUserStore{
+		client: client,
+		coll:   client.Database(dbname).Collection(userColl),
+	}
+}
+
+func (s *MongoUserStore) Drop(ctx context.Context) error {
+	fmt.Println("--- dropping user collection")
+	return s.coll.Drop(ctx)
+}
+
+func (s *MongoUserStore) UpdateUser(ctx context.Context, filter Map, params types.UpdateUserParams) error {
+	oid, err := primitive.ObjectIDFromHex(filter["_id"].(string))
+	if err != nil {
+		return err
+	}
+	filter["_id"] = oid
+	update := bson.M{"$set": params.ToBSON()}
+	_, err = s.coll.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *MongoUserStore) DeleteUser(ctx context.Context, id string) error {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	// TODO: Maybe its a good idea to handle if we did not delete any user.
+	// maybe log it or something??
+	_, err = s.coll.DeleteOne(ctx, bson.M{"_id": oid})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *MongoUserStore) InsertUser(ctx context.Context, user *types.User) (*types.User, error) {
+	res, err := s.coll.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	user.ID = res.InsertedID.(primitive.ObjectID)
+	return user, nil
+}
+
+func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
+	cur, err := s.coll.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	var users []*types.User
+	if err := cur.All(ctx, &users); err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+func (s *MongoUserStore) GetUserByEmail(ctx context.Context, email string) (*types.User, error) {
+	var user types.User
+	if err := s.coll.FindOne(ctx, bson.M{"email": email}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+	var user types.User
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+/*
+What's happening here?
+perfoming the actions we want on database regarding user
+we want to add a user, get it by id, get all, delete one, update or more
+so first connect to database and pass it to the function responsible
+for doing each task and perfrom query and finally return the result
+
+Language specfic things done here
+1. Interface : To make the code less coupled,
+defining the methods that we would need of regardless of database
+so that we don't have to rewrite it for every new database
+these are, get user, add user, getUserbyID and all
+so if we change the db to postgres, the methods remains the same
+
+UNKNOWNS?
+1. const userColl = "users" -> what is it and why?
+2. type Map map[string]any -> what's the use of map?
+Ans : map[string]any is a way to send the response in meaningful ways, if json way "name" : "la"
+3. what is (s *MongoUserStore) sent to function called? and why?
+
+*/
